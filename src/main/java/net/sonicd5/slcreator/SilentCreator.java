1691ea83b67bf259af1e4c369f663bec2c97e8ea
SonicD5 <mixey3112009@gmail.com>
2024-08-26 19:42:42 +0400

    Initial commit

---
+++ src/main/java/net/sonicd5/slcreator/SilentCreator.java
@@ -0,0 +1,14 @@
+package net.sonicd5.slcreator;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SilentCreator implements ModInitializer {
+	public static final String MOD_ID = "slcreator";
+	public static final Logger LOGGER = LoggerFactory.getLogger("Silent Creator");
+
+	@Override
+	public void onInitialize() {}
+}
---
+++ src/main/java/net/sonicd5/slcreator/SilentCreatorDataGenerator.java
@@ -0,0 +1,11 @@
+package net.sonicd5.slcreator;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SilentCreatorDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
